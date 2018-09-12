e2f58c9728fd1b080adfe9f4035d92b90d969849
Szymon Miks <miks.szymon@gmail.com>
2018-09-12 22:54:19 +0200

    added Post model to my blog-site, added migration to .gitignore

--- django_blog_site/blog/views.py
+++ django_blog_site/blog/views.py
@@ -1,26 +1,12 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
+from .models import Post
 
 
 # Create your views here.
 def home(request):
     context = {
         'title': "Home page title",
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
