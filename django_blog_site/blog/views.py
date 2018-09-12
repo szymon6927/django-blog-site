9ea87d2ec7a92515af609a2cd517adcd13f7b9f0
Szymon Miks <miks.szymon@gmail.com>
2018-09-12 20:22:50 +0200

    added blog app with some basic templates and routes

---
+++ django_blog_site/blog/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
+
+# Create your views here.
+def home(request):
+    context = {
+        'title': "Home page title",
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html')
